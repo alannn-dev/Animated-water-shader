9211475c50a4bdd5596dbe4153133232f0167bba
Alan Boulay <boulay.alan@gmail.com>
2026-01-19 13:41:07 +0700

    Speed waves control (+ uBigWavesSpeed uniforms) added

--- src/script.js
+++ src/script.js
@@ -33,7 +33,9 @@ const waterMaterial = new THREE.ShaderMaterial({
         uTime: { value: 0},
         uBigWavesElevation: { value: 0.2},
         // For different frequency control on X and Y.
-        uBigWavesFrequency: { value: new THREE.Vector2(4, 1.5) }
+        uBigWavesFrequency: { value: new THREE.Vector2(4, 1.5) },
+        // Waves speed
+        uBigWavesSpeed: { value: 0.75}
     }
 })
 
@@ -41,6 +43,7 @@ const waterMaterial = new THREE.ShaderMaterial({
 gui.add(waterMaterial.uniforms.uBigWavesElevation, 'value').min(0).max(1).step(0.001).name('uBigWavesElevation');
 gui.add(waterMaterial.uniforms.uBigWavesFrequency.value, 'x').min(0).max(10).step(0.001).name('uBigWavesFrequencyX');
 gui.add(waterMaterial.uniforms.uBigWavesFrequency.value, 'y').min(0).max(10).step(0.001).name('uBigWavesFrequencyY');
+gui.add(waterMaterial.uniforms.uBigWavesSpeed, 'value').min(0).max(10).step(0.001).name('uBigWavesSpeed');
 
 // Mesh
 const water = new THREE.Mesh(waterGeometry, waterMaterial)
@@ -102,7 +105,7 @@ const tick = () =>
 
     // Update water
     waterMaterial.uniforms.uTime.value = elapsedTime
-    
+
     // Update controls
     controls.update()
 
