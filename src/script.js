041986f8daa10cc192d224ade89be253d8807806
Alan Boulay <boulay.alan@gmail.com>
2026-01-19 12:42:36 +0700

    Different waves on X & Y axis added (With uniforms & sin())

--- src/script.js
+++ src/script.js
@@ -29,9 +29,16 @@ const waterMaterial = new THREE.ShaderMaterial({
     fragmentShader: waterVertexFragment,
     uniforms: 
     {
-        uBigWavesElevation: { value: 0.2}
+        uBigWavesElevation: { value: 0.2},
+        // For different frequency control on X and Y.
+        uBigWavesFrequency: { value: new THREE.Vector2(4, 1.5) }
     }
 })
+
+// Debug
+gui.add(waterMaterial.uniforms.uBigWavesElevation, 'value').min(0).max(1).step(0.001).name('uBigWavesElevation');
+gui.add(waterMaterial.uniforms.uBigWavesFrequency.value, 'x').min(0).max(10).step(0.001).name('uBigWavesFrequencyX');
+gui.add(waterMaterial.uniforms.uBigWavesFrequency.value, 'y').min(0).max(10).step(0.001).name('uBigWavesFrequencyY');
 
 // Mesh
 const water = new THREE.Mesh(waterGeometry, waterMaterial)
